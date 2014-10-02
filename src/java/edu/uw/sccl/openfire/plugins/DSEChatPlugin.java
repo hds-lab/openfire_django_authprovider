79c3495417d8843c84420d67b3653b6a7ec286c7
Michael Brooks <mjbrooks@uw.edu>
2014-10-02 15:09:39 -0700

    plugin setup

---
+++ src/java/edu/uw/sccl/openfire/plugins/DSEChatPlugin.java
@@ -0,0 +1,19 @@
+package edu.uw.sccl.openfire.plugins;
+
+import org.jivesoftware.openfire.container.Plugin;
+import org.jivesoftware.openfire.container.PluginManager;
+import java.io.File;
+
+/**
+ * A sample plugin for Openfire.
+ */
+public class DSEChatPlugin implements Plugin {
+
+    public void initializePlugin(PluginManager manager, File pluginDirectory) {
+        // Your code goes here
+    }
+
+    public void destroyPlugin() {
+        // Your code goes here
+    }
+}
